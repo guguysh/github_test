6da5976275c2c2752a6dc6b53c865a6f3edb3165
ysh <ysh.turing@gmail.com>
2024-08-26 14:32:03 +0800

    module2

--- github_test/src/main/java/com/kscs/Main.java
+++ github_test/src/main/java/com/kscs/Main.java
@@ -3,5 +3,6 @@ package com.kscs;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("引出了module2分支！");
     }
 }
